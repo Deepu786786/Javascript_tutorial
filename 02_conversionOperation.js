d64000b9d5fa6eb0605c83397536f2a7d9f819f9
Deepu786786 <76244496+Deepu786786@users.noreply.github.com>
2023-09-21 13:16:50 +0530

    Create 02_conversionOperation.js

---
+++ 02_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+// let score = null
+// let score = undefined
+
+console.log(typeof(score));
+
+score = Number(score);
+
+console.log(typeof(score));
+console.log(score);
+
+let x = null
+
+console.log(typeof x)
+
+x = Boolean(x)
+
+console.log(typeof x)
+console.log(x)
