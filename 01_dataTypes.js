aa3e47f72b8133a8304e6d60d3f863c078781b9c
Deepu786786 <76244496+Deepu786786@users.noreply.github.com>
2023-09-21 13:16:12 +0530

    Create 01_dataTypes.js

---
+++ 01_dataTypes.js
@@ -0,0 +1,41 @@
+"use strict" // it will treat all JS code as newer version
+
+// alert (3+3) // it is node.js not browser
+
+//Documentation to read about Javascript
+/*
+    mdn web docs
+    tc39.es (offcial mozilla documentation)
+*/
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => for unique types
+// object
+
+
+//Primitive datatypes
+//String, Number, Boolean, null, undefined, Symbol, BigInt
+//Stack memoery is used
+
+//Reference (Non primitive)
+//Array, Objects, Functions
+//Heap memory is used
+
+
+const myArray = [1,2,3];
+
+
+const myObject = {
+    name: "Deepanshu",
+    age: 23,
+};
+
+
+const myFunction = function(){
+    console.log("Hello world");
+};
