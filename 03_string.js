a1a58d64742eab134257133d9a2a73ffaa0ca66d
Deepu786786 <76244496+Deepu786786@users.noreply.github.com>
2023-09-21 13:17:14 +0530

    Create 03_string.js

---
+++ 03_string.js
@@ -0,0 +1,10 @@
+let Name = "Deepanshu"; //one way of defining a string
+
+let age = new String("23"); //another way of defining strings
+
+//Outdated way of concatenation
+console.log("Hello my name is " + name + " and my age is " + 23);
+
+
+//Latest way called string interpolation method
+console.log(`Hello my name is ${Name} and my age is ${age}`);
